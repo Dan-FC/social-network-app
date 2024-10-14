cebdeecfa666657c4491d2d1f9c25641bfdcc437
Frank0125 <a01252974@tec.mx>
2024-10-13 18:19:03 -0700

    UserLogin Initializer

---
+++ app/components/UserLogIn.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const UserLogIn = () => {
+  return (
+    <View>
+        <Text>Log In</Text>
+    </View>
+  );
+};
+
+export default UserLogIn;
--- app/tabs/AllPost.tsx
+++ app/tabs/AllPost.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
+import UserLogIn from "../components/UserLogIn";
 
 const AllPost = () => {
-  return <Text>All Posts</Text>;
+  return <Text>
+            <UserLogIn />
+    All Posts</Text>;
 };
 
 export default AllPost;
